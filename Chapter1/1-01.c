fcfd6d557e2ccd8fc22fc9c0d474f03a2835e586
shields1 <isak@shields.nu>
2023-07-08 18:50:30 +0200

    modified:   Chapter1/1-01.c

--- Chapter1/1-01.c
+++ Chapter1/1-01.c
@@ -4,7 +4,6 @@
  *     Run the "Hello world" program on your system.
  *     Experiment with leaving out parts of the program, to see what 
  *     error messages you get.
- * 
  */
 int main() {
 	printf("Hello world!\n");
